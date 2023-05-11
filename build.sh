8456997a3375ace0df80ef269a43362716395eac
AlexStorm <info@alexstorm.ru>
2023-05-11 16:44:43 +0530

    fix domain

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   # rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.klogsolenopsys.org/api/charts
+  curl --data-binary "@${FN}" http://helm.solenopsys.org/api/charts
 }
 
-REGISTRY=registry.klogsolenopsys.org
+REGISTRY=registry.solenopsys.org
 NAME=alexstorm-hsm-postgres
 ARCHS="linux/amd64,linux/arm64"
 VER=0.1.3
